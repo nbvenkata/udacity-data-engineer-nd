31890c17dde8da1f4725d273eaf4846c514b01e0
Naveen BV <naveenbv80@gmail.com>
2020-03-03 18:50:19 -0600

    Project#4

---
+++ Project # 4/plugins/plugins/operators/stage_redshift.py
@@ -0,0 +1,61 @@
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+
+class StageToRedshiftOperator(BaseOperator):
+    ui_color = '#358140'
+    template_fields = ("s3_key",)
+    
+    @apply_defaults
+    def __init__(self,
+              redshift_conn_id = ‘’,
+              table_name = '',
+              s3_bucket = '',
+              s3_key = '',
+              path = '',
+              delimiter = '',
+              headers = 1,
+              quote_char = '',
+              file_type = '',
+              aws_credentials = {},
+              region = '',
+              *args, **kwargs):
+
+        super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
+        self.table_name = table_name
+        self.redshift_conn_id = redshift_conn_id
+        self.s3_bucket = s3_bucket
+        self.s3_key = s3_key
+        self.path = path
+        self.delimiter = delimiter
+        self.headers = headers
+        self.quote_char = quote_char
+        self.file_type = file_type
+        self.aws_credentials = aws_credentials
+        self.region = region        
+           
+
+    def execute(self, context):
+        self.log.info('StageToRedshiftOperator not implemented yet')
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        rendered_key = self.s3_key.format(**context)
+        s3_path = 's3://{}/{}'.format(self.s3_bucket, rendered_key)
+        print(s3_path)
+        
+copy_statement = """
+                    copy {} 
+                    from '{}'
+                    access_key_id '{}'
+                    secret_access_key '{}'
+                    region as '{}'
+                """.format(self.table_name, s3_path, self.aws_credentials.get('key'), self.aws_credentials.get('secret'), self.region)
+        if self.file_type == 'csv':
+            file_statement = """
+                        delimiter '{}'
+                        ignoreheader {}
+                        csv quote as '{}';
+                    """.format(self.delimiter, self.headers, self.quote_char)
+
+
+
+
