6d63679606bfbcbef58d7c6a295447bec81a5083
Naveen BV <naveenbv80@gmail.com>
2020-03-04 16:13:07 -0600

    Update project#4

--- Project # 4/airflow/airflow/dags/udac_example_dag.py
+++ Project # 4/airflow/airflow/dags/udac_example_dag.py
@@ -44,11 +44,11 @@ stage_events_to_redshift = StageToRedshiftOperator(
     s3_bucket='udacity-dend',
     s3_key='log_data/2018/11/{ds}-events.json',
     path = 's3://udacity-dend/log_json_path.json',
-   delimiter=',',
-   headers='1',
-   quote_char='"',
-   file_type='json',
-   aws_credentials={
+    delimiter=',',
+    headers='1',
+    quote_char='"',
+    file_type='json',
+    aws_credentials={
         'key': AWS_KEY,
         'secret': AWS_SECRET
     },
@@ -74,9 +74,69 @@ stage_songs_to_redshift = StageToRedshiftOperator(
     region = 'us-west-2'
 )
 
+load_songplays_table = LoadFactOperator(
+    task_id='Load_songplays_fact_table',
+    dag=dag,
+    redshift_conn_id="redshift",
+    table="songplays",
+    sql_statement=SqlQueries.songplay_table_insert,
+    append_data=False
+)
+
+load_user_dimension_table = LoadDimensionOperator(
+    task_id='Load_user_dim_table',
+    dag=dag,
+    redshift_conn_id="redshift",
+    table="users",
+    sql_statement=SqlQueries.user_table_insert,
+    append_data=False
+)
+
+load_song_dimension_table = LoadDimensionOperator(
+    task_id='Load_song_dim_table',
+    dag=dag,
+    redshift_conn_id="redshift",
+    table="songs",
+    sql_statement=SqlQueries.song_table_insert,
+    append_data=False
+)
+
+load_artist_dimension_table = LoadDimensionOperator(
+    task_id='Load_artist_dim_table',
+    dag=dag,
+    redshift_conn_id="redshift",
+    table="artists",
+    sql_statement=SqlQueries.artist_table_insert,
+    append_data=False    
+)
+
+load_time_dimension_table = LoadDimensionOperator(
+    task_id='Load_time_dim_table',
+    dag=dag,
+    redshift_conn_id="redshift",
+    table="time",
+    sql_statement=SqlQueries.time_table_insert,
+    append_data=False     
+)
+
+run_quality_checks = DataQualityOperator(
+    task_id='Run_data_quality_checks',
+    dag=dag,
+    redshift_conn_id="redshift",
+    dg_checks=[
+        {'check_sql': "SELECT COUNT(*) FROM users WHERE userid is null", 'expected_result': 0}
+        {'check_sql': "SELECT COUNT(*) FROM songs WHERE userid is null", 'expected_result': 0}
+   ]     
+)
+
 end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
 
 # Task dependencies
-start_operator >> stage_events_to_redshift >> end_operator
-start_operator >> stage_songs_to_redshift >> end_operator
+start_operator >> stage_events_to_redshift >> load_songplays_table
+start_operator >> stage_songs_to_redshift >> load_songplays_table
+load_songplays_table >> load_user_dimension_table >> run_quality_checks
+load_songplays_table >> load_song_dimension_table >> run_quality_checks
+load_songplays_table >> load_artist_dimension_table >> run_quality_checks
+load_songplays_table >> load_time_dimension_table >> run_quality_checks
+run_quality_checks >> end_operator
 
--- Project # 4/airflow/airflow/plugins/operators/load_fact.py
+++ Project # 4/airflow/airflow/plugins/operators/load_fact.py
@@ -11,12 +11,22 @@ class LoadFactOperator(BaseOperator):
                  # Define your operators params (with defaults) here
                  # Example:
                  # conn_id = your-connection-name
+                 redshift_conn_id='',
+                 table_name='',
+                 sql_statement='',
                  *args, **kwargs):
 
         super(LoadFactOperator, self).__init__(*args, **kwargs)
         # Map params here
         # Example:
         # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id
+        self.table_name = table_name
+        self.sql_statement = sql_statement
 
     def execute(self, context):
-        self.log.info('LoadFactOperator not implemented yet')
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        self.log.info('Starting to load fact table %s' % self.table_name)
+        sql_statement = 'INSERT INTO %s %s' % (self.table_name, self.sql_statement)
+        redshift.run(sql_statement)
+        self.log.info('Fact table %s load finished' % self.table_name)
